aadc8d9f17687e64743b99b2b282377b02a38870
Dz298 <dz298@cornell.edu>
2020-12-11 23:36:18 -0600

    edit SBD

--- Satellite com (UART)/IridiumSBD.c
+++ Satellite com (UART)/IridiumSBD.c
@@ -22,8 +22,8 @@
  * Input: buffer - contents to write
  * Returns: if the operation succeeds
  */
-bool ConsoleWrite( void *buffer){
-  bool status = UART_Write(buffer, sizeof(buffer));
+bool send( void *buffer){
+  bool status = UART0_Write(buffer, sizeof(buffer));
   return status;
 }
 
@@ -45,74 +45,7 @@ If "prompt" string is provided (example "+CSQ:"), then all characters following
 prompt up to the next CRLF are stored in response buffer for later parsing by caller.
  */
 bool waitForATResponse(IridiumSBD* self,char *response=NULL, int responseSize=0, const char *prompt=NULL, const char *terminator="OK\r\n"){
-  diagprint(F("Waiting for response "));
-     diagprint(terminator);
-     diagprint(F("\r\n"));
-
-     if (response)
-        memset(response, 0, responseSize);
-
-     int matchPromptPos = 0; // Matches chars in prompt
-     int matchTerminatorPos = 0; // Matches chars in terminator
-     enum {LOOKING_FOR_PROMPT, GATHERING_RESPONSE, LOOKING_FOR_TERMINATOR};
-     int promptState = prompt ? LOOKING_FOR_PROMPT : LOOKING_FOR_TERMINATOR;
-     consoleprint(F("<< "));
-     for (unsigned long start=millis(); millis() - start < 1000UL * atTimeout;)
-     {
-        if (cancelled())
-           return false;
-
-        while (filteredavailable() > 0)
-        {
-           char c = filteredread();
-           if (prompt)
-           {
-              switch (promptState)
-              {
-              case LOOKING_FOR_PROMPT:
-                 if (c == prompt[matchPromptPos])
-                 {
-                    ++matchPromptPos;
-                    if (prompt[matchPromptPos] == '\0')
-                       promptState = GATHERING_RESPONSE;
-                 }
-
-                 else
-                 {
-                    matchPromptPos = c == prompt[0] ? 1 : 0;
-                 }
-
-                 break;
-              case GATHERING_RESPONSE: // gathering response from end of prompt to first \r
-                 if (response)
-                 {
-                    if (c == '\r' || responseSize < 2)
-                    {
-                       promptState = LOOKING_FOR_TERMINATOR;
-                    }
-                    else
-                    {
-                       *response++ = c;
-                       responseSize--;
-                    }
-                 }
-                 break;
-              }
-           }
-
-           if (c == terminator[matchTerminatorPos])
-           {
-              ++matchTerminatorPos;
-              if (terminator[matchTerminatorPos] == '\0')
-                 return true;
-           }
-           else
-           {
-              matchTerminatorPos = c == terminator[0] ? 1 : 0;
-           }
-        } // while (stream.available() > 0)
-     } // timer loop
-     return false;
+   
 }
 
 /*
@@ -121,15 +54,23 @@ Input: void
 Returns: if the operation succeeds
  */
 bool disableFlowControl(void){
-  bool status = ConsoleWrite("AT&K0\r");
-
+  bool status = send("AT&K0\r");
+  return status 
 }
 
 int sendSBDText(IridiumSBD* self,const char *message){
 
+
 }
 int sendSBDBinary(IridiumSBD* self,const uint8_t *txData, size_t txDataSize){
+   send("AT+SBDWB=[");
+   send(txDataSize);// todo : int to string
+   send("]\r");
 
+   // wait for READY\r
+   
+   // send txData + checksum 
+   // wait for 0\r\n\r\nOK\r\n
 }
 int sendReceiveSBDText(IridiumSBD* self,const char *message, uint8_t *rxBuffer, size_t &rxBufferSize){
 
