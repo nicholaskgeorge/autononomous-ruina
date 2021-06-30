23a1ca9b22a66fc4f64f52fc3206faef39ed0ad8
nicholaskgeorge <nkg37@cornell.edu>
2021-06-29 20:26:20 -0400

    added zero padding for messages and sending angle command demonstration

---
+++ Radio(USART)/BoatCommunicate.py
@@ -0,0 +1,39 @@
+import serial
+import sys
+from time import sleep
+ser = serial.Serial(port='COM7',baudrate=19200, timeout=0.5)
+
+def Radio_Encode(message):
+    encode = message[:]
+    lengthbit = len(message)+3
+    encode = bytearray([lengthbit,0x20])+encode
+    encode += bytearray([0x00,0x00])
+    return encode
+
+def Radio_decode(message):
+    decode = message[11:len(message)-1]
+    return decode.decode("utf-8")
+
+def main():
+    rec = ''
+    switch = True
+    message = "/$CP:DA,0.0%"
+    message += (119-len(message))*' '
+    send = Radio_Encode(message.encode('utf-8'))
+    # print(send)
+    ser.write(send)
+    # if ser.isOpen():
+    #     print("open")
+    #     while True :
+    #         n = ser.inWaiting()
+    #         if n :
+    #             sleep(0.09)
+    #             n = ser.inWaiting()
+    #             data = ser.read(n)
+    #             print(Radio_decode(data))
+    # else:
+    #     print(" not open")
+
+
+if __name__ == '__main__':
+    main()
