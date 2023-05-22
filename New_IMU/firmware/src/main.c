145f199bea522d50a511b520715e12fea028c7ae
someoneAW <103203008+someoneAW@users.noreply.github.com>
2023-05-22 17:05:54 -0400

    code working
    The data saving issue fixed

--- New_IMU/firmware/src/main.c
+++ New_IMU/firmware/src/main.c
@@ -2,18 +2,21 @@
 #include <stddef.h>                     // Defines NULL
 #include <stdbool.h>                    // Defines true
 #include <stdlib.h> 
-//#include "read_new_imu.h"
-//
-//uint8_t data_buffer[buff_size];
-//NEW_IMU_INFO* new_imu_info;
+#include "read_new_imu.h"
+
+uint8_t data_buffer[buff_size];
+NEW_IMU_INFO* new_imu_info;
+int save_val;
+double yaw;
 
 int main ( void )
 {
+    SYS_Initialize ( NULL );
     while(1){
-        printf("hi");
-//        USART1_Read(data_buffer, buff_size);
-//        New_IMU_Process(new_imu_info, data_buffer);
-//        printf("%f" , new_imu_info->yaw);
+        USART1_Read(data_buffer, buff_size);
+        New_IMU_Process(new_imu_info, data_buffer);
+        yaw = new_imu_info->yaw;
+        printf("Yaw:%f ,Pitch:%f, Roll:%f\n" , new_imu_info->yaw, new_imu_info->pitch, new_imu_info->roll);
     }
 }
 
--- New_IMU/firmware/src/read_new_imu.c
+++ New_IMU/firmware/src/read_new_imu.c
@@ -4,6 +4,13 @@
 #include <stdbool.h>                    // Defines true
 #include <stdlib.h> 
 
+
+double imu_process(uint8_t low_byte, uint8_t high_byte){
+    double val1 = high_byte<<8 | low_byte;
+    double val2 = val1/32768*180;
+    return val2;
+}
+
 void New_IMU_Process(NEW_IMU_INFO* IMU_info, uint8_t* data){
    uint8_t* ptr = data;
    uint8_t low_byte = 0;
@@ -11,28 +18,29 @@ void New_IMU_Process(NEW_IMU_INFO* IMU_info, uint8_t* data){
 
    //move past the header
    ptr += 2;
-
+   
+   
    //Roll
    low_byte = *ptr;
    high_byte = *(ptr+1);
-   IMU_info->roll = low_byte | high_byte<<8;
+   IMU_info->roll = imu_process(low_byte, high_byte);
    //move to next value
    ptr += 2;
 
    //Pitch
    low_byte = *ptr;
    high_byte = *(ptr+1);
-   IMU_info->pitch = low_byte | high_byte<<8;
+   IMU_info->pitch = imu_process(low_byte, high_byte);
+   ptr += 2;
    //move to next value
 
    //Yaw
    low_byte = *ptr;
    high_byte = *(ptr+1);
-   IMU_info->yaw = low_byte | high_byte<<8;
-   //move to next value
-   ptr += 2;
+   IMU_info->yaw = imu_process(low_byte, high_byte);
 
    //the last bytes of the message contain the tempurature and the
    //check sum which we do not care about
 
 }
+
